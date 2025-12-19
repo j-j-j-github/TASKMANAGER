68177dd04f25f0b888f93d64d0b9c4a82ca00067
j-j-j-github <jeevaljolly@gmail.com>
2025-12-19 12:49:29 +0530

    DB Upgrade

--- obj/Debug/net8.0/TASKMANAGER.AssemblyInfo.cs
+++ obj/Debug/net8.0/TASKMANAGER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TASKMANAGER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239e7777f7a9de86694f1f6a041ef26d76c393ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e23d134076bda3e1202ccfa97b3e243fa5658575")]
 [assembly: System.Reflection.AssemblyProductAttribute("TASKMANAGER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TASKMANAGER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
